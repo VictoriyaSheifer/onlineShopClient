4da3906000f588fbfc519ed3d4307736dc35865f
victoriya sheifer <victoriya2275@gmail.com>
2021-10-20 17:47:34 +0300

    Update Images

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -6,13 +6,14 @@ import { Injectable } from '@angular/core';
 export class SettingsService {
   version: string = "1.0.1";
   baseUrl: string = "https://demoonlineshop.herokuapp.com";
+  ImageUrl: string = "https://res.cloudinary.com/drzzptile/image/upload/";
   userId: number = 0
   currentPage: number = 0;
   public slides = [
-    { src: "http://www.localhost:5000/carousel_commercials/commercial_pic1.png" },
-    { src: "http://www.localhost:5000/carousel_commercials/commercial_pic2.png" },
-    { src: "http://www.localhost:5000/carousel_commercials/commercial_pic3.png" },
-    { src: "http://www.localhost:5000/carousel_commercials/commercial_pic4.png" }
+    { src: "https://res.cloudinary.com/drzzptile/image/upload/v1634739775/online_shop/carousel_commercials/commercial_pic1.png" },
+    { src: "https://res.cloudinary.com/drzzptile/image/upload/v1634739775/online_shop/carousel_commercials/commercial_pic2.png" },
+    { src: "https://res.cloudinary.com/drzzptile/image/upload/v1634739775/online_shop/carousel_commercials/commercial_pic3.png" },
+    { src: "https://res.cloudinary.com/drzzptile/image/upload/v1634739775/online_shop/carousel_commercials/commercial_pic4.png" }
   ];
 
 
