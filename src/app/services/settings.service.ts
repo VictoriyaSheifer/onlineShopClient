0d62ea8dda9c3141b13ed3f8402969e0a3947ee5
victoriya sheifer <victoriya2275@gmail.com>
2021-10-20 20:02:24 +0300

    Fix image path in service files

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -10,10 +10,10 @@ export class SettingsService {
   userId: number = 0
   currentPage: number = 0;
   public slides = [
-    { src: "{{this.settingsService.ImageUrl}}v1634739775/online_shop/carousel_commercials/commercial_pic1_minery.png" },
-    { src: "{{this.settingsService.ImageUrl}}v1634739775/online_shop/carousel_commercials/commercial_pic2_kj3lhq.png" },
-    { src: "{{this.settingsService.ImageUrl}}v1634739775/online_shop/carousel_commercials/commercial_pic3_oaj1qn.png" },
-    { src: "{{this.settingsService.ImageUrl}}v1634739775/online_shop/carousel_commercials/commercial_pic4_zsdvva.png" }
+    { src: `${this.ImageUrl}v1634739775/online_shop/carousel_commercials/commercial_pic1_minery.png` },
+    { src: `${this.ImageUrl}v1634739775/online_shop/carousel_commercials/commercial_pic2_kj3lhq.png` },
+    { src: `${this.ImageUrl}v1634739775/online_shop/carousel_commercials/commercial_pic3_oaj1qn.png` },
+    { src: `${this.ImageUrl}v1634739775/online_shop/carousel_commercials/commercial_pic4_zsdvva.png` }
   ];
 
 
